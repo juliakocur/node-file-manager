be09e1dfb3fa3ad5fa2a614db08682ff13b0007e
Julia Kocur <juliakocur@mail.com>
2025-04-17 16:59:52 +0200

    feat: add Operation failed message

--- src/handle.js
+++ src/handle.js
@@ -1,9 +1,31 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 export const handleCommand = (input) => {
     const [command, ...args] = input.split(' ');
 
     switch (command) {
+        case 'cd':
+            if (args.length !== 1) {
+                throw new Error('Invalid input');
+            }
+        changeDirectory(args[0]);
+        break;
 
         default:
         console.error('Invalid input');
     }
 };
+
+const changeDirectory = (input) => {
+    const newPath = path.isAbsolute(input) ? input : path.join(process.cwd(), input);
+    try {
+        if (fs.existsSync(newPath) && fs.statSync(newPath).isDirectory()) {
+            process.chdir(newPath);
+        } else {
+            throw new Error();
+        }
+    } catch {
+        console.log('Operation failed');
+    }
+}
